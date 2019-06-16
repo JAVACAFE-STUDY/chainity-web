8814835e72e7513bca57a15c5e2f84da4cdc236b
kyungseop <kyungseop@thebettertech.co.kr>
2019-06-16 15:51:31 +0900

    1. 이벤트 등록 후 이벤트 리스트 화면으로 이동 2. 이벤트 참여 수 표시 안되는 현상 수정

--- src/main/webapp/app/components/card/home-status.tsx
+++ src/main/webapp/app/components/card/home-status.tsx
@@ -6,10 +6,9 @@ import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 import { getSession } from 'app/shared/reducers/authentication';
 import { getGroup } from 'app/pages/group/groups.reducer';
-import { getEventParticipations, getEvents } from 'app/pages/events/event.reducer';
+import { getEventParticipations, getEvents, getGroupParticipations } from 'app/pages/events/event.reducer';
 import { getUsers } from 'app/pages/users/users.reducer';
 import _ from 'lodash';
-import { Link } from "react-router-dom";
 
 const styles = {
     card: {
@@ -39,11 +38,13 @@ export class HomeStatus extends React.Component<IHomeStatusProp> {
         this.props.getGroup('1');
         this.props.getEvents('1');
         this.props.getUsers('1');
+        this.props.getGroupParticipations('1');
     }
 
     render() {
-        const { classes, group, events, participations, users } = this.props;
-        console.log('group, events, participations, users  => ', group, events, participations, users);
+        const { classes, group, events, groupParticipations, users } = this.props;
+        console.log('group, events, participations, users  => ', group, events, users);
+        console.log('groupParticipations => ', groupParticipations);
         let initialTokens = 0;
         let usedTokens = 0;
         let name = '';
@@ -59,37 +60,37 @@ export class HomeStatus extends React.Component<IHomeStatusProp> {
             <Card>
                 <CardContent>
                     <Typography variant="h5" component="h2" color="textSecondary" gutterBottom>
-                      <img src={ '/content/images/logo.png' } className={ classes.logo } />{ name } 상태
+                        <img src={ '/content/images/logo.png' } className={ classes.logo }/>{ name } 상태
                     </Typography>
-                    <Typography variant="subtitle1" className={classes.subtitle} component="p">
+                    <Typography variant="subtitle1" className={ classes.subtitle } component="p">
                         총 토큰 발생
                     </Typography>
                     <Typography component="p">
                         { initialTokens }
                     </Typography>
-                    <Typography variant="subtitle1" className={classes.subtitle} component="p">
+                    <Typography variant="subtitle1" className={ classes.subtitle } component="p">
                         토큰 보상
                     </Typography>
                     <Typography component="p">
                         { usedTokens }
                     </Typography>
-                    <Typography variant="subtitle1" className={classes.subtitle}component="p">
+                    <Typography variant="subtitle1" className={ classes.subtitle } component="p">
                         사용자 수
                     </Typography>
                     <Typography component="p">
                         { users.totalDocs }
                     </Typography>
-                    <Typography variant="subtitle1" className={classes.subtitle} component="p">
+                    <Typography variant="subtitle1" className={ classes.subtitle } component="p">
                         이벤트 수
                     </Typography>
                     <Typography component="p">
                         { events.totalDocs }
                     </Typography>
-                    <Typography variant="subtitle1" className={classes.subtitle} component="p">
+                    <Typography variant="subtitle1" className={ classes.subtitle } component="p">
                         참여 수
                     </Typography>
                     <Typography component="p">
-                        { participations.length }
+                        { groupParticipations.totalDocs }
                     </Typography>
                 </CardContent>
             </Card>
@@ -100,11 +101,11 @@ export class HomeStatus extends React.Component<IHomeStatusProp> {
 const mapStateToProps = storeState => ({
     group: storeState.groups.group,
     events: storeState.event.events,
-    participations: storeState.event.participations,
+    groupParticipations: storeState.event.groupParticipations,
     users: storeState.users.data
 });
 
-const mapDispatchToProps = { getSession, getGroup, getEvents, getEventParticipations, getUsers };
+const mapDispatchToProps = { getSession, getGroup, getEvents, getGroupParticipations, getEventParticipations, getUsers };
 
 type StateProps = ReturnType<typeof mapStateToProps>;
 type DispatchProps = typeof mapDispatchToProps;
